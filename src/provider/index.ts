7d86809c481a80799361c5979fa44967275c75bd
nicodealmirante <nicolas.e.luna@gmail.com>
2024-01-19 15:32:12 -0300

    Update index.ts

--- src/provider/index.ts
+++ src/provider/index.ts
@@ -1,4 +1,4 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
-import ProviderWS from '@bot-whatsapp/provider/baileys'
+import ProviderWS from '@bot-whatsapp/provider/meta'
 
 export default BotWhatsapp.createProvider(ProviderWS)
