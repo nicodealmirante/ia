e325d2ab0291891573a5969b177cc0c56e70d5d9
nicodealmirante <nicolas.e.luna@gmail.com>
2024-01-18 23:09:59 -0300

    Update index.ts

--- src/flow/index.ts
+++ src/flow/index.ts
@@ -1,7 +1,6 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
 import helloFlow from './hello.flow';
 import welcomeFlow from './welcome.flow';
-import chatbotFlow from './chatbot.flow';
 import nodeFlow from './node.flow';
 
 /**
