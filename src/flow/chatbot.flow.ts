562c030c8bfe4d026bc60bfd475134837d87f7d9
nicodealmirante <nicolas.e.luna@gmail.com>
2024-01-18 23:27:48 -0300

    Update chatbot.flow.ts

--- src/flow/chatbot.flow.ts
+++ src/flow/chatbot.flow.ts
@@ -1,5 +1,4 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
-import { generatePaymentLink } from 'src/services/paypal';
 
 /**
  * Un flujo conversacion que responder a las palabras claves "hola", "buenas", ...
@@ -16,7 +15,6 @@ export default BotWhatsapp.addKeyword(BotWhatsapp.EVENTS.ACTION)
     .addAnswer('...generando link de pago de curso de chatbot')
     .addAction(async (ctx, {flowDynamic, state}) => {
         const email = state.get('email')
-        const paypalLink = await generatePaymentLink('30.00', email)
-        await flowDynamic(paypalLink)
+
     })
 
