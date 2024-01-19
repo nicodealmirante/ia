448924981888c8dbd91e90cc7022331e43d464ee
nicodealmirante <nicolas.e.luna@gmail.com>
2024-01-19 00:30:01 -0300

    Update index.ts

--- src/flow/index.ts
+++ src/flow/index.ts
@@ -1,12 +1,10 @@
 import BotWhatsapp from '@bot-whatsapp/bot';
-import helloFlow from './hello.flow';
 import welcomeFlow from './welcome.flow';
 /**
  * Debes de implementasr todos los flujos
  */
 export default BotWhatsapp.createFlow(
     [
-        helloFlow,
         welcomeFlow,
     ]
 )
